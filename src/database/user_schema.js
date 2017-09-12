a33e481f3f823420a717e82787d344b826ae6cf2
matthewdking <matthew.d.king@outlook.com>
2017-09-12 10:49:18 +0300

    update user schema relates #8 #14 #16

--- src/database/user_schema.js
+++ src/database/user_schema.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String, required: true}
+  userDetails: {
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+  },
+  goals: [
+    {
+      goal90: {
+        name: {type: String, required: true},
+        dailyGoal: {type: String, required: true},
+        description: {type: String, required: true},
+        reward: {type: String, required: true},
+        endDate: {type: String, required: true}
+      }
+    }
+  ]
 });
 
 const users = mongoose.model('user', userSchema);
