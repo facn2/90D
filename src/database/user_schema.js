1a91319eabf7e7580188298da59956d3921f8c92
Wyrd00 <amy.tai0120@gmail.com>
2017-09-13 14:57:51 +0300

    deleted commented out codes

--- src/database/user_schema.js
+++ src/database/user_schema.js
@@ -5,17 +5,6 @@ const userSchema = mongoose.Schema({
   lastName: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required: true}
-  // goals: [
-  //   {
-  //     goal90: {
-  //       name: {type: String, required: true},
-  //       dailyGoal: {type: String, required: true},
-  //       description: {type: String, required: true},
-  //       reward: {type: String, required: true},
-  //       endDate: {type: String, required: true}
-  //     }
-  //   }
-  // ]
 });
 
 const users = mongoose.model('user', userSchema);
