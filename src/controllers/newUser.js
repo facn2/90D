f52608fac55d95d9b5896cd1c49fef76a7b1c1e9
Wyrd00 <amy.tai0120@gmail.com>
2017-09-13 18:00:37 +0300

    forget it, we\'re doing client side validation + fixed package

--- src/controllers/newUser.js
+++ src/controllers/newUser.js
@@ -5,20 +5,9 @@ module.exports = (req, res) => {
   console.log(req.body);
   let userData = req.body;
 
-  // req.checkBody('firstName', 'Invalid name').isAlpha().notEmpty();;
-  // req.checkBody('lastName', 'Invalid name').isAlpha();
-  // req.sanitize('firstName').escape().trim();
-
-  // let errors = req.getValidationResult();
-  // if (errors) {
-  //   //If there are errors render the form again, passing the previously entered values and errors
-  //   res.render('signup', { title: 'Sign-Up', firstName: firstName, lastName: lastName, email: email, password: password, errors: errors});
-  //   return;
-  // }
-
   if (!userData.firstName || !userData.lastName || !userData.email || !userData.password) {
     res.render('error', {
-      message: 'Sorry, hold on, let me give you a better error later', type: 'error'
+      message: 'Sorry, your user details is a little lacking. King hates you.', type: 'error'
     });
   }
 
