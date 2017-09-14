095aca107a0aca1b78849aa7b0b0cb1a05afbaa3
matthewdking <matthew.d.king@outlook.com>
2017-09-14 11:08:09 +0300

    start validating login relates #3 #45

--- src/controllers/validateLogin.js
+++ src/controllers/validateLogin.js
@@ -1,3 +1,12 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (req, res) => {
-  // do some stuff
+  console.log(req.body);
+  let userData = req.body;
+  if (!userData.email || !userData.password) {
+    res.render('error', {
+      message: 'Sorry, your user details is a little lacking. King hates you.', type: 'error'
+    });
+  }
+  res.redirect('/login');
 };
