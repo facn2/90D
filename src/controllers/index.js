bcbcb97bc67165c2237853854929c76e5e3b6510
Wyrd00 <amy.tai0120@gmail.com>
2017-09-17 15:18:14 +0300

    add goal relates #56 #55

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,6 +6,7 @@ const newUser = require('./newUser');
 const login = require('./login');
 const validate = require('./validateLogin');
 const newGoal = require('./newGoal');
+const addGoal = require('./addGoal');
 const error = require('./error');
 
 router.get('/signup', signup);
@@ -13,6 +14,7 @@ router.post('/newUser', newUser);
 router.get('/login', login);
 router.post('/validateLogin', validate);
 router.get('/newGoal', newGoal);
+router.post('/addGoal', addGoal);
 
 router.use(error.client);
 router.use(error.server);
--- src/database/goal_schema.js
+++ src/database/goal_schema.js
@@ -7,7 +7,7 @@ const goalSchema = mongoose.Schema({
       dailyGoal: {type: String, required: true},
       description: {type: String, required: true},
       reward: {type: String, required: true},
-      endDate: {type: String, required: true}
+      endDate: {type: Date, default: Date.now}
     }
   }
 });
