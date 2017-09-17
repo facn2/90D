f7194b96831c27eb17d4ec416df0f88c15c2f63e
Wyrd00 <amy.tai0120@gmail.com>
2017-09-17 17:04:37 +0300

    solves issues with add new goal relates #65

--- src/controllers/addGoal.js
+++ src/controllers/addGoal.js
@@ -2,29 +2,38 @@ const { goal } = require('../database/goal_schema');
 
 module.exports = (req, res) => {
   let userGoal = req.body;
-  if (!userGoal.ninetyDays || !userGoal.dailyGoal || !userGoal.description || !userGoal.reward) {
+  console.log(6, res.locals);
+  console.log(2, userGoal.goal90);
+  console.log(3, userGoal.dailyGoal);
+  console.log(4, userGoal.description);
+  console.log(5, userGoal.reward);
+
+  if (!userGoal.goal90 || !userGoal.dailyGoal || !userGoal.description || !userGoal.reward) {
+    console.log('fuck')
     res.render('error', {
       message: 'Gah', type: 'error'
     });
+  } else {
+    let newGoal = new goal({
+      goals: {
+        goal90: req.body.goal90,
+        dailyGoal: req.body.dailyGoal,
+        description: req.body.description,
+        reward: req.body.reward
+      }
+    });
+
+    console.log(newGoal);
+
+    newGoal.save((err) => {
+      if (err) {
+        console.log(1, err);
+        res.render('error', {
+          message: 'Sorry, your ambitions are breaking up with you. It\'s not you, it\'s them', type: 'error'
+        });
+      } else {
+        return res.redirect('/');
+      }
+    });
   }
-  console.log(req.body);
-
-  let newGoal = new goal({
-    name: req.body.ninetyDays,
-    dailyGoal: req.body.dailyGoal,
-    description: req.body.description,
-    reward: req.body.reward
-    // endDate: req.body.date
-  });
-
-  newGoal.save((err) => {
-    if (err) {
-      console.log(err);
-      res.render('error', {
-        message: 'Sorry, your ambitions are breaking up with you. It\'s not you, it\'s them', type: 'error'
-      });
-    } else {
-      res.redirect('/');
-    }
-  });
 };
