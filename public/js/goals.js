2a72f133d0199638a19ae5f630d6df7d09cc26a7
matthewdking <matthew.d.king@outlook.com>
2017-09-18 15:24:05 +0300

    removes console logs and fixes append child relates #77

--- public/js/goals.js
+++ public/js/goals.js
@@ -2,14 +2,12 @@ function dateDiffInDays (a, b) {
   // Discard the time and time-zone information.
   var millisecondsPerDay = 1000 * 60 * 60 * 24;
   var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  console.log('This is utc1: ', utc1);
   var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
   return Math.floor((utc2 - utc1) / millisecondsPerDay);
 }
 
 // get todays date
 var today = new Date();
-console.log('This is the date now: ', typeof today);
 // get end date
 var goalList = document.getElementById('goalList');
 var goals = goalList.children;
@@ -19,5 +17,5 @@ Object.keys(goals).forEach((key) => {
   var dayDiff = dateDiffInDays(today, endDate);
   var endDateItem = document.createElement('li');
   endDateItem.innerText = 'Days left: ' + dayDiff;
-  goalList.appendChild(endDateItem);
+  goal.appendChild(endDateItem);
 });
