f7eccd882cc9dce4125120da6cc6031c6488e54e
Wyrd00 <amy.tai0120@gmail.com>
2017-09-18 16:43:04 +0300

    add function for unchecking

--- src/controllers/updateGoal.js
+++ src/controllers/updateGoal.js
@@ -3,13 +3,25 @@ const { Goal } = require('../database/goal_schema');
 module.exports = (req, res) => {
   console.log(1, req.body);
   const dailyGoal = req.body.dailyGoal;
-  Goal.update({'dailyGoal': dailyGoal},
-    { '$set': {'dailyCheck': true}, '$inc': {'dailyCounter': +1}
-    }, (err, result) => {
-      if (err) {
-        console.log(34, err);
-      } else {
-        res.redirect('/goals');
-      }
-    });
+  if (req.body.completion) {
+    Goal.update({'dailyGoal': dailyGoal},
+      { '$set': {'dailyCheck': true}, '$inc': {'dailyCounter': +1}
+      }, (err, result) => {
+        if (err) {
+          console.log(34, err);
+        } else {
+          res.redirect('/goals');
+        }
+      });
+  } else {
+    Goal.update({'dailyGoal': dailyGoal},
+      { '$set': {'dailyCheck': false}, '$inc': {'dailyCounter': -1}
+      }, (err, result) => {
+        if (err) {
+          console.log(34, err);
+        } else {
+          res.redirect('/goals');
+        }
+      });
+  }
 };
