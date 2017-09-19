ddac87f680652ab2468a28ec728d9e66253a924f
matthewdking <matthew.d.king@outlook.com>
2017-09-19 12:42:32 +0300

    adds basic tests for / relates #3 #46

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -12,13 +12,30 @@ describe('login', () => {
       .get('/')
       .expect(200, done);
   });
-
-  it('response header - no error & not empty', (done) => {
+  it('response header should have keys', (done) => {
     request(app)
       .get('/')
       .end((err, res) => {
         should.not.exist(err);
         expect(res.headers).to.be.an('object').to.have.any.keys('content-type', 'content-length', 'etag', 'date', 'connection');
+        done();
+      });
+  });
+  it('response text', (done) => {
+    request(app)
+      .get('/')
+      .end((err, res) => {
+        should.not.exist(err);
+        expect(res.text).to.be.an('string').to.include('<label for="email" class="clip">Email</label>');
+        done();
+      });
+  });
+  it('response body', (done) => {
+    request(app)
+      .get('/')
+      .end((err, res) => {
+        should.not.exist(err);
+        expect(res.body).to.be.empty.to.be.an('object');
         done();
       });
   });
